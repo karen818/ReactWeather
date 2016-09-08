f60c7bb0db0b2b94ce0f2395d9e28ae578941550
karen818 <klc0818@outlook.com>
2016-09-08 12:38:11 -0500

    style about page and link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,27 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page</p>
+      <h1 className='text-center'>About</h1>
+        <div className="media-object">
+          <div className="media-object-section">
+            <div className="thumbnail">
+              <img src= "http://www.fillmurray.com/g/100/100"/>
+            </div>
+          </div>
+          <div className="media-object-section">
+            <h4>Dreams feel real while we're in them.</h4>
+            <p>I'm going to improvise.</p>
+          </div>
+        </div>
+        <ul>
+          <li>
+            <a href="https://facebook.github.io/react">React</a> -- This is the JavaScript framework used
+          </li>
+          <li>
+            <a href="http://openweathermap.org">Open Weather Map</a> -- This is the API used to look up weather info
+          </li>
+
+        </ul>
     </div>
   )
 };
